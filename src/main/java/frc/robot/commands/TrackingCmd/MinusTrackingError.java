1438c0051eef61fbdca9380577abf91a35844f1b
jeremy1214 <s1110280@my.cmsh.cyc.edu.tw>
2024-01-28 22:31:05 +0800

    [add] all folder

---
+++ src/main/java/frc/robot/commands/TrackingCmd/MinusTrackingError.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.TrackingCmd;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivetain;
+
+public class MinusTrackingError extends Command {
+  /** Creates a new MinusTrackingError. */
+  private final Drivetain drivetain;
+
+  public MinusTrackingError(Drivetain drivetain) {
+    this.drivetain = drivetain;
+    addRequirements(drivetain);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    drivetain.minusTrackTargetError();
+  }
+}
