9142691414027b5e0f07bdb93e5b1a5a70152ef4
jeremy1214 <s1110280@my.cmsh.cyc.edu.tw>
2024-02-06 09:54:25 +0800

    [del] static

--- src/main/java/frc/robot/subsystems/AprilTag.java
+++ src/main/java/frc/robot/subsystems/AprilTag.java
@@ -8,40 +8,40 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.AprilTagConstants;
 
 public class AprilTag extends SubsystemBase {
-    public static NetworkTable table;
-    public static NetworkTableEntry tx;// = table.getEntry("tx");// table.getEntry("tx");
-    public static NetworkTableEntry ty;// = table.getEntry("ty");
-    public static NetworkTableEntry ta;// = table.getEntry("ta");
-    public static NetworkTableEntry tv;
-    public static NetworkTableEntry tid;
-    public static NetworkTableEntry tl;
+    public NetworkTable table;
+    public NetworkTableEntry tx;// = table.getEntry("tx");// table.getEntry("tx");
+    public NetworkTableEntry ty;// = table.getEntry("ty");
+    public NetworkTableEntry ta;// = table.getEntry("ta");
+    public NetworkTableEntry tv;
+    public NetworkTableEntry tid;
+    public NetworkTableEntry tl;
 
-    public static NetworkTableEntry BT;
-    public static NetworkTableEntry CR;
-    public static NetworkTableEntry TR;
-    public static NetworkTableEntry CT;
+    public NetworkTableEntry BT;
+    public NetworkTableEntry CR;
+    public NetworkTableEntry TR;
+    public NetworkTableEntry CT;
 
-    public static double v;
-    public static double x;
-    public static double y;
-    public static double area;
-    public static double ID;
+    public double v;
+    public double x;
+    public double y;
+    public double area;
+    public double ID;
     // public static double cl;
-    public static double latency;
+    public double latency;
 
-    public static double[] bt; // botpose_targetspace
-    public static double[] cr;// camerapose_robotspace
-    public static double[] tr; // targetpose_robotpose;
-    public static double[] ct; // camerapose_targetspace
+    public double[] bt; // botpose_targetspace
+    public double[] cr;// camerapose_robotspace
+    public double[] tr; // targetpose_robotpose;
+    public double[] ct; // camerapose_targetspace
 
-    public static double distance;
-    public static double MyDistance;
+    public double distance;
+    public double MyDistance;
 
     // Write to constants
 
-    public static double goalHeightInches;
+    public double goalHeightInches;
 
-    public static void init() {
+    public void init() {
         table = NetworkTableInstance.getDefault().getTable("limelight");
         tx = table.getEntry("tx");// table.getEntry("tx");
         ty = table.getEntry("ty");
@@ -57,7 +57,7 @@ public class AprilTag extends SubsystemBase {
 
     }
 
-    public static void change_APipeline() {
+    public void change_APipeline() {
         table.getEntry("pipeline").setNumber(AprilTagConstants.A_pipeline);
     }
 
@@ -66,7 +66,7 @@ public class AprilTag extends SubsystemBase {
     //     return meters;
     // }
 
-    public static void readValue() {
+    public void readValue() {
         v = tv.getDouble(0);
         x = tx.getDouble(0.0);
         y = tx.getDouble(0.0);
@@ -79,7 +79,7 @@ public class AprilTag extends SubsystemBase {
         cr = CR.getDoubleArray(new double[6]);
     }
 
-    public static double getMyDistance() {
+    public double getMyDistance() {
 
         readValue();
         double target_height = bt[1]; // botpose in targetspace y
@@ -96,7 +96,7 @@ public class AprilTag extends SubsystemBase {
         return tx;
       }
 
-    public static void putDashboard() {
+    public void putDashboard() {
         readValue();
         SmartDashboard.putNumber("hasTarget", v);
         SmartDashboard.putNumber("LimelightX", x);
