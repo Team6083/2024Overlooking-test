d4acd7d06bca2d7604540c825d59605e1ffba4e4
AYao1026 <95smoonplasticworkuse@gmail.com>
2024-02-17 18:36:53 +0800

    [add] updatePoseEstimatorWithVisionBotPose() in AprilTag

--- src/main/java/frc/robot/subsystems/AprilTag.java
+++ src/main/java/frc/robot/subsystems/AprilTag.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.VecBuilder;
 // import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -96,10 +97,51 @@ public class AprilTag extends SubsystemBase {
         return bt;
     }
 
-    public double[] getCT(){
+    public double[] getCT() {
         ct = table.getEntry("camerapose_targetspace").getDoubleArray(new double[6]);
         return ct;
     }
+
+    // public void updatePoseEstimatorWithVisionBotPose() {
+    //     PoseLatency visionBotPose = m_visionSystem.getPoseLatency();
+    //     // invalid LL data
+    //     if (visionBotPose.pose2d.getX() == 0.0) {
+    //         return;
+    //     }
+
+    //     // distance from current pose to vision estimated pose
+    //     double poseDifference = m_poseEstimator.getEstimatedPosition().getTranslation()
+    //             .getDistance(visionBotPose.pose2d.getTranslation());
+
+    //     if (m_visionSystem.areAnyTargetsValid()) {
+    //         double xyStds;
+    //         double degStds;
+    //         // multiple targets detected
+    //         if (m_visionSystem.getNumberOfTargetsVisible() >= 2) {
+    //             xyStds = 0.5;
+    //             degStds = 6;
+    //         }
+    //         // 1 target with large area and close to estimated pose
+    //         else if (m_visionSystem.getBestTargetArea() > 0.8 && poseDifference < 0.5) {
+    //             xyStds = 1.0;
+    //             degStds = 12;
+    //         }
+    //         // 1 target farther away and estimated pose is close
+    //         else if (m_visionSystem.getBestTargetArea() > 0.1 && poseDifference < 0.3) {
+    //             xyStds = 2.0;
+    //             degStds = 30;
+    //         }
+    //         // conditions don't match to add a vision measurement
+    //         else {
+    //             return;
+    //         }
+
+    //         m_poseEstimator.setVisionMeasurementStdDevs(
+    //                 VecBuilder.fill(xyStds, xyStds, Units.degreesToRadians(degStds)));
+    //         m_poseEstimator.addVisionMeasurement(visionBotPose.pose2d,
+    //                 Timer.getFPGATimestamp() - visionBotPose.latencySeconds);
+    //     }
+    // }
 
     public void putDashboard() {
         // SmartDashboard.putNumber("hasTarget", getTv());
--- src/main/java/frc/robot/subsystems/drive/Drivebase.java
+++ src/main/java/frc/robot/subsystems/drive/Drivebase.java
@@ -6,6 +6,7 @@ package frc.robot.subsystems.drive;
 
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
